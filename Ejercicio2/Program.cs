2287306027622261de3518d2da97e6811d3eb9de
LuisMartinez1 <34143705+LuisMartinez1@users.noreply.github.com>
2023-03-22 19:26:20 -0600

    Commint initial

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var depositos = new List<Deposito>()
+            {
+                new Deposito("Ana", 50),
+                new Deposito("Paco", 10),
+                new Deposito("Ana", 20),
+                new Deposito("Jorge", 55),
+                new Deposito("Laura", 75),
+                new Deposito("Laura", 3),
+                new Deposito("Laura", 50),
+            };
+
+            var grupos = from d in depositos
+                         group d by d.Titular into g
+                         where g.Count() > 1 && g.Average(d => d.Monto) > 50
+                         select new
+                         {
+                             Titular = g.Key,
+                             Minimo = g.Min(d => d.Monto),
+                             Maximo = g.Max(d => d.Monto),
+                             Cantidad = g.Count(),
+                             Promedio = g.Average(d => d.Monto),
+                         };
+
+            foreach (var grupo in grupos)
+            {
+                Console.WriteLine($"Titular: {grupo.Titular}");
+                Console.WriteLine($"Monto mínimo: {grupo.Minimo}");
+                Console.WriteLine($"Monto máximo: {grupo.Maximo}");
+                Console.WriteLine($"Cantidad de depósitos: {grupo.Cantidad}");
+                Console.WriteLine($"Promedio de depósitos: {grupo.Promedio}");
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Deposito
+    {
+        public string Titular { get; set; }
+        public decimal Monto { get; set; }
+
+        public Deposito(string titular, decimal monto)
+        {
+            Titular = titular;
+            Monto = monto;
+        }
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un número entero: ");
+            string input = Console.ReadLine();
+
+            int n;
+            if (!int.TryParse(input, out n))
+            {
+                Console.WriteLine("El valor ingresado no es un número entero válido.");
+                return;
+            }
+            int result = Fibonacci(n);
+            Console.WriteLine("Fib({0}) = {1}", n, result);
+        }
+
+        static int Fibonacci(int n)
+        {
+            int[] memo = new int[n + 1];
+            return Fibonacci(n, memo);
+        }
+
+        static int Fibonacci(int n, int[] memo)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            if (memo[n] == 0)
+            {
+                memo[n] = Fibonacci(n - 1, memo) + Fibonacci(n - 2, memo);
+            }
+
+            return memo[n];
+        }
+    }
+}
